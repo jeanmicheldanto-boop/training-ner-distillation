2870a2133bd4509ad226fcd7e4e1f3c717cfa527
jeanmicheldanto-boop <jeanmichel.danto@gmail.com>
2025-11-10 16:50:27 +0100

    fix: Convert string labels to IDs during tokenization

--- train_distillation.py
+++ train_distillation.py
@@ -68,8 +68,10 @@ def tokenize_and_align_labels(examples):
             if word_idx is None:
                 label_ids.append(-100)
             elif word_idx != previous_word_idx:
-                label_ids.append(label[word_idx])
+                label_str = label[word_idx]
+                label_ids.append(label2id[label_str])
             else:
+                # C'est un sous-mot, on met -100 aussi
                 label_ids.append(-100)
             previous_word_idx = word_idx
         labels.append(label_ids)
